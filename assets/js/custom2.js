3b67ada1065a1357618ca7357c7ba4478e27f98f
Rajib Sarker <redhat0593@gmail.com>
2023-09-04 00:45:42 +0600

    s-menu edited

---
+++ assets/js/custom2.js
@@ -0,0 +1,7 @@
+function hoverMenu(target) {
+    document.getElementById(target).classList.add("show");
+}
+
+function hoverMenuOut(target) {
+    document.getElementById(target).classList.remove("show");
+}
