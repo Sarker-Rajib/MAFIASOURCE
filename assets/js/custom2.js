b1280d96580a54109658dbe371f53bf4f882da99
Rajib Sarker <redhat0593@gmail.com>
2023-09-04 01:10:38 +0600

    done 3

--- assets/js/custom2.js
+++ assets/js/custom2.js
@@ -1,7 +1,7 @@
-function hoverMenu(target) {
-    document.getElementById(target).classList.add("show");
-}
+// function hoverMenu(target) {
+//     document.getElementById(target).classList.add("show");
+// }
 
-function hoverMenuOut(target) {
-    document.getElementById(target).classList.remove("show");
-}
+// function hoverMenuOut(target) {
+//     document.getElementById(target).classList.remove("show");
+// }
