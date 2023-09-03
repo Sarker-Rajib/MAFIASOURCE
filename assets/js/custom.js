6237e8eea9eea876e5d27a42b1bf63d85fcad050
Rajib Sarker <redhat0593@gmail.com>
2023-09-03 20:17:50 +0600

    things are done

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+document.getElementById("btOpen").addEventListener('click', () => {
+    document.getElementById("btOpen").classList.toggle("d-none");
+    document.getElementById("prog-report").classList.toggle("d-none");
+    document.getElementById("coinData").classList.toggle("d-none");
+    document.getElementById("btClose").classList.toggle("d-none");
+})
+
+document.getElementById("btClose").addEventListener('click', () => {
+    document.getElementById("btClose").classList.toggle("d-none");
+    document.getElementById("prog-report").classList.toggle("d-none");
+    document.getElementById("coinData").classList.toggle("d-none");
+    document.getElementById("btOpen").classList.toggle("d-none");
+})
+
+document.getElementById("trigger-nva").addEventListener('click', () => {
+    document.getElementById("dropIng").classList.toggle("d-none");
+})
