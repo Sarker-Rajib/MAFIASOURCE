d9d9dc36ba16674fb5a78af70c01bad64ed8c8de
Rajib Sarker <redhat0593@gmail.com>
2023-09-07 15:58:02 +0600

    mobile nav created manualy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,6 @@
-// document.getElementById("btOpen").addEventListener('click', () => {
-//     document.getElementById("btOpen").classList.toggle("d-none");
-//     document.getElementById("prog-report").classList.toggle("d-none");
-//     document.getElementById("coinData").classList.toggle("d-none");
-//     document.getElementById("btClose").classList.toggle("d-none");
-// })
-
-// document.getElementById("btClose").addEventListener('click', () => {
-//     document.getElementById("btClose").classList.toggle("d-none");
-//     document.getElementById("prog-report").classList.toggle("d-none");
-//     document.getElementById("coinData").classList.toggle("d-none");
-//     document.getElementById("btOpen").classList.toggle("d-none");
-// })
-
-// document.getElementById("trigger-nva").addEventListener('click', () => {
-//     const cNav = document.getElementById("dropIng");
-
-//     if (cNav.style.display === "none") {
-//         cNav.style.display = "block";
-//     } else {
-//         cNav.style.display = "none";
-//     }
-// })
+document.getElementById("openTrigger").addEventListener('click', () => {
+    document.getElementById("resNav").classList.add("show")
+})
+document.getElementById("closeTrigger").addEventListener('click', () => {
+    document.getElementById("resNav").classList.remove("show")
+})
